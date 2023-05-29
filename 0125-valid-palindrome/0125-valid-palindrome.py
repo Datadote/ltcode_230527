f6bfc7505d6874314203f8cabb0eda466ce02efa
Daniel L <datadote@gmail.com>
2023-05-29 15:25:32 -0700

    Time: 54 ms (68.55%), Space: 16.9 MB (53.89%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,15 +1,59 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        alpha_num = 'abcdefghijklmnopqrstuvwxyz0123456789'
+        alph_num = 'abcdefghijklmnopqrstuvwrxyz0123456789'
         tmp = ''
         s = s.lower()
         for c in s:
-            if c in alpha_num:
+            if c in alph_num:
                 tmp += c
         L, R = 0, len(tmp)-1
-        while L < R:
+        while L<R:
             if tmp[L] != tmp[R]:
                 return False
             L += 1
             R -= 1
         return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # alpha_num = 'abcdefghijklmnopqrstuvwxyz0123456789'
+        # tmp = ''
+        # s = s.lower()
+        # for c in s:
+        #     if c in alpha_num:
+        #         tmp += c
+        # L, R = 0, len(tmp)-1
+        # while L < R:
+        #     if tmp[L] != tmp[R]:
+        #         return False
+        #     L += 1
+        #     R -= 1
+        # return True
