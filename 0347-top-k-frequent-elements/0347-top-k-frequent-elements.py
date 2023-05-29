98281750f503964517b75ffdee94931371359588
Daniel L <datadote@gmail.com>
2023-05-29 13:46:47 -0700

    Time: 101 ms (93.37%), Space: 21.3 MB (20.72%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,11 +3,9 @@ class Solution:
         d = defaultdict(int)
         for n in nums:
             d[n] += 1
-            
         topk = sorted(d.items(), key = lambda x: x[1])[::-1]
         topk = [x[0] for x in topk[:k]]
         return topk
-        
         
         # count = Counter(nums)
         # topk = [ele for ele,_ in count.most_common(k)]
