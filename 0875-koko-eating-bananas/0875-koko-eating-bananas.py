61fc5bdaf4bc76cfd50d465d2121a5730a529550
Daniel L <datadote@gmail.com>
2023-06-02 12:30:42 -0700

    Time: 461 ms (70.24%), Space: 17.9 MB (24.32%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -5,15 +5,63 @@ class Solution:
             for p in piles:
                 hrs += math.ceil(p/k)
             return hrs
-        
         L, R = 1, max(piles)
-        min_k = R
+        mink = R
         while L <= R:
             M = L + (R-L)//2
             hrs = num_hrs(piles, M)
-            if hrs <= h:
-                min_k = min(min_k, M)
-                R = M - 1
-            else:
+            if hrs > h:
                 L = M + 1
-        return min_k
+            else:
+                mink = min(mink, M)
+                R = M - 1
+        return mink
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         def num_hrs(piles, k):
+#             hrs = 0
+#             for p in piles:
+#                 hrs += math.ceil(p/k)
+#             return hrs
+        
+#         L, R = 1, max(piles)
+#         min_k = R
+#         while L <= R:
+#             M = L + (R-L)//2
+#             hrs = num_hrs(piles, M)
+#             if hrs <= h:
+#                 min_k = min(min_k, M)
+#                 R = M - 1
+#             else:
+#                 L = M + 1
+#         return min_k
