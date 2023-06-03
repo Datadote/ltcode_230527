f8b01ce6120ca1cf5dbc83e4801c5b556f9b99bc
Daniel L <datadote@gmail.com>
2023-06-03 13:06:30 -0700

    Time: 235 ms (71.12%), Space: 25.2 MB (20.77%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -13,39 +13,49 @@ class Solution:
         for i in range(len(nums)):
             nums[i] = pre[i] * post[i+1]
         return nums
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+        # pre, post = [1], [1]
+        # tmp = 1
+        # for n in nums:
+        #     tmp *= n
+        #     pre.append(tmp)
+        # tmp = 1
+        # for n in nums[::-1]:
+        #     tmp *= n
+        #     post.append(tmp)
+        # post = post[::-1]
+        # for i in range(len(nums)):
+        #     nums[i] = pre[i] * post[i+1]
+        # return nums
         
         # pre, post = [1], [1]
         # tmp = 1
