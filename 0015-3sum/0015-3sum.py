8395dc0b11597898cddfb8f61b84221516893727
Daniel L <datadote@gmail.com>
2023-06-03 13:36:56 -0700

    Time: 1455 ms (61.85%), Space: 20.4 MB (47.00%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,46 +1,12 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
         res = []
         nums.sort()
         for i in range(len(nums)-2):
-            if nums[i] > 0:
-                break
             if i>0 and nums[i]==nums[i-1]:
                 continue
-            
             L, R = i+1, len(nums)-1
-            while L < R:
+            while L<R:
                 total = nums[i] + nums[L] + nums[R]
                 if total > 0:
                     R -= 1
@@ -49,9 +15,60 @@ class Solution:
                 else:
                     res.append([nums[i], nums[L], nums[R]])
                     L += 1
-                    while L<R and nums[L]==nums[L-1]:
+                    while L<R and nums[L] == nums[L-1]:
                         L += 1
-        return res 
+        return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         res = []
+#         nums.sort()
+#         for i in range(len(nums)-2):
+#             if nums[i] > 0:
+#                 break
+#             if i>0 and nums[i]==nums[i-1]:
+#                 continue
+            
+#             L, R = i+1, len(nums)-1
+#             while L < R:
+#                 total = nums[i] + nums[L] + nums[R]
+#                 if total > 0:
+#                     R -= 1
+#                 elif total < 0:
+#                     L += 1
+#                 else:
+#                     res.append([nums[i], nums[L], nums[R]])
+#                     L += 1
+#                     while L<R and nums[L]==nums[L-1]:
+#                         L += 1
+#         return res 
         
 #         res = []
 #         nums.sort() # python sort = mergesort variant = O(n)
