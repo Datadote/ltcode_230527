70cb3668ba7ee0a61c7123bc1565144c531d2464
Daniel L <datadote@gmail.com>
2023-06-03 12:29:59 -0700

    Time: 87 ms (39.48%), Space: 16.7 MB (36.81%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,17 +1,61 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
+        dmap = {'+': lambda a,b: a+b,
+                '-': lambda a,b: a-b,
+                '*': lambda a,b: a*b,
+                '/': lambda a,b: int(a/b)
+               }
         stack = []
-        dmap = {
-            '+': lambda a,b: a+b,
-            '-': lambda a,b: a-b,
-            '*': lambda a,b: a*b,
-            '/': lambda a,b: int(a/b)
-        }
-        
-        for token in tokens:
-            if token in dmap:
-                b, a = stack.pop(), stack.pop()
-                stack.append(dmap[token](a,b))
+        for c in tokens:
+            if c not in dmap:
+                stack.append(int(c))
             else:
-                stack.append(int(token))
+                b, a = stack.pop(), stack.pop()
+                stack.append(dmap[c](a,b))
         return stack[0]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         stack = []
+#         dmap = {
+#             '+': lambda a,b: a+b,
+#             '-': lambda a,b: a-b,
+#             '*': lambda a,b: a*b,
+#             '/': lambda a,b: int(a/b)
+#         }
+        
+#         for token in tokens:
+#             if token in dmap:
+#                 b, a = stack.pop(), stack.pop()
+#                 stack.append(dmap[token](a,b))
+#             else:
+#                 stack.append(int(token))
+#         return stack[0]
