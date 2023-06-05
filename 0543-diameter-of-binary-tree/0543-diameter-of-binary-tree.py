f008c1953f8a0b98490d960cf3528d6abacec843
Daniel L <datadote@gmail.com>
2023-06-05 11:54:16 -0700

    Time: 53 ms (67.36%), Space: 18.7 MB (36.28%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.py
@@ -13,7 +13,7 @@ class Solution:
             left = dfs(root.left)
             right = dfs(root.right)
             res[0] = max(res[0], 2 + left + right)
-            return 1 + max(left, right)
+            return 1 + max(left,right)
         dfs(root)
         return res[0]
         
@@ -44,6 +44,19 @@ class Solution:
         
         
         
+        
+        
+        # res = [0]
+        # def dfs(root):
+        #     if not root:
+        #         return -1
+        #     left = dfs(root.left)
+        #     right = dfs(root.right)
+        #     res[0] = max(res[0], 2 + left + right)
+        #     return 1 + max(left, right)
+        # dfs(root)
+        # return res[0]
+    
         # res = [0]
         # def dfs(root):
         #     if not root:
