4d17bd82ce9e071310202115beb85983ae003d76
Daniel L <datadote@gmail.com>
2023-06-05 12:23:39 -0700

    Time: 92 ms (7.66%), Space: 17.4 MB (27.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,14 +1,13 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        alp_num = set('abcdefghijklmnopqrstuvwxyz0123456789')
+        alp_num = 'abcdefghijklmnopqrstuvwrxyz0123456789'
         L, R = 0, len(s)-1
-        while L <= R:
-            if s[L].lower() not in alp_num:
+        while L < R:
+            while L<R and s[L].lower() not in alp_num:
                 L += 1
-                continue
-            if s[R].lower() not in alp_num:
+            while L<R and s[R].lower() not in alp_num:
                 R -= 1
-                continue
+            print(s[L].lower(), s[R].lower())
             if s[L].lower() != s[R].lower():
                 return False
             L += 1
@@ -42,6 +41,25 @@ class Solution:
         
         
         
+        
+        
+        
+        
+        
+        # alp_num = set('abcdefghijklmnopqrstuvwxyz0123456789')
+        # L, R = 0, len(s)-1
+        # while L <= R:
+        #     if s[L].lower() not in alp_num:
+        #         L += 1
+        #         continue
+        #     if s[R].lower() not in alp_num:
+        #         R -= 1
+        #         continue
+        #     if s[L].lower() != s[R].lower():
+        #         return False
+        #     L += 1
+        #     R -= 1
+        # return True
         
         # alph_num = 'abcdefghijklmnopqrstuvwrxyz0123456789'
         # L, R = 0, len(s)-1
