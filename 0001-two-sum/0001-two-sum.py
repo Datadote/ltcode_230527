ce7d4743dceb124bda419a1cfe0d17686868286b
Daniel L <datadote@gmail.com>
2023-06-06 08:56:21 -0700

    Time: 79 ms (54.75%), Space: 17.7 MB (11.57%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(len(nums)):
             diff = target - nums[i]
             if diff in d:
-                return [i, d[diff]]
+                return (i, d[diff])
             d[nums[i]] = i
         
         # d = {}
