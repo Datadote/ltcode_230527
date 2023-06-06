1dda9718884d9c4ba88400b401a8cb032bb05ac2
Daniel L <datadote@gmail.com>
2023-06-06 09:12:58 -0700

    Time: 45 ms (93.98%), Space: 16.9 MB (26.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,11 +1,13 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        d1, d2 = {}, {}
-        for c in s:
-            d1[c] = d1.get(c, 0) + 1
-        for c in t:
-            d2[c] = d2.get(c, 0) + 1
-        return d1 == d2
+        return Counter(s) == Counter(t)
+        
+        # d1, d2 = {}, {}
+        # for c in s:
+        #     d1[c] = d1.get(c, 0) + 1
+        # for c in t:
+        #     d2[c] = d2.get(c, 0) + 1
+        # return d1 == d2
         
         
         
