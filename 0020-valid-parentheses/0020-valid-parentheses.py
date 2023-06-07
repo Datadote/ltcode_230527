92b63b0d2cda5a8fbc9b78bdd24bfd6483213573
Daniel L <datadote@gmail.com>
2023-06-07 09:31:08 -0700

    Time: 56 ms (6.88%), Space: 16.4 MB (6.29%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,13 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        dcheck = {')':'(', ']':'[', '}':'{'}
         stack = []
+        dR2L = {']': '[', '}': '{', ')': '('}
         for c in s:
-            if c in dcheck and stack:
-                if stack.pop() != dcheck[c]:
-                    return False
-            else:
+            if c not in dR2L:
                 stack.append(c)
+            elif stack and dR2L[c] == stack.pop():
+                continue
+            else:
+                return False
         return len(stack) == 0
         
         
@@ -38,6 +39,19 @@ class Solution:
         
         
         
+        
+        
+        
+        
+        # dcheck = {')':'(', ']':'[', '}':'{'}
+        # stack = []
+        # for c in s:
+        #     if c in dcheck and stack:
+        #         if stack.pop() != dcheck[c]:
+        #             return False
+        #     else:
+        #         stack.append(c)
+        # return len(stack) == 0
         
         # stack = []
         # dR2L = {')':'(', ']':'[', '}':'{'}
