43996c2b5ce46fd2bf122cd8110bffa740e44236
Daniel L <datadote@gmail.com>
2023-06-08 10:59:46 -0700

    Time: 47 ms (54.91%), Space: 16.4 MB (69.88%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -12,3 +12,49 @@ class Solution:
             dfs(i+1)
         dfs(0)
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # res = []
+        # subset = []
+        # def dfs(i):
+        #     if i == len(nums):
+        #         res.append(subset.copy())
+        #         return
+        #     subset.append(nums[i])
+        #     dfs(i+1)
+        #     subset.pop()
+        #     dfs(i+1)
+        # dfs(0)
+        # return res
