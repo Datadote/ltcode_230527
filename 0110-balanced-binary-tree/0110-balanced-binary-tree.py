e8964df138fe07036f8c5e7091065e5963e2c796
Daniel L <datadote@gmail.com>
2023-06-08 11:54:09 -0700

    Time: 58 ms (75.85%), Space: 21.4 MB (12.43%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -11,54 +11,56 @@ class Solution:
                 return [True, 0]
             left = dfs(root.left, h)
             right = dfs(root.right, h)
-            balanced = (left[0]
+            balanced = (abs(left[1] - right[1]) < 2
+                        and left[0]
                         and right[0]
-                        and abs(left[1] - right[1]) < 2
                        )
             return [balanced, 1 + max(left[1], right[1])]
         return dfs(root, 0)[0]
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    # def max_depth(root):
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # def dfs(root, h):
         #     if not root:
-        #         return 0
-        #     left = max_depth(root.left)
-        #     right = max_depth(root.right)
-        #     if abs(left-right) > 1:
-        #         return False
-        #     return 1 + max(left, right)
-        # if not root or max_depth(root):
-        #     return True
-        # else:
-        #     return False
+        #         return [True, 0]
+        #     left = dfs(root.left, h)
+        #     right = dfs(root.right, h)
+        #     balanced = (left[0]
+        #                 and right[0]
+        #                 and abs(left[1] - right[1]) < 2
+        #                )
+        #     return [balanced, 1 + max(left[1], right[1])]
+        # return dfs(root, 0)[0]
