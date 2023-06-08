e0ca951fabd801d3a317786a85aa6012122fff18
Daniel L <datadote@gmail.com>
2023-06-08 10:33:44 -0700

    Time: 57 ms (21.78%), Space: 17.8 MB (59.45%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -12,3 +12,43 @@ class Solution:
             prev = curr
             curr = nxt
         return prev
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # prev, curr = None, head
+        # while curr:
+        #     nxt = curr.next
+        #     curr.next = prev
+        #     prev = curr
+        #     curr = nxt
+        # return prev
