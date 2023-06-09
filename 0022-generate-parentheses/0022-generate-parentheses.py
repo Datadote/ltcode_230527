d1a1596c94f65457ff2a61a52b826f6bd7d78aa8
Daniel L <datadote@gmail.com>
2023-06-09 14:10:50 -0700

    Time: 56 ms (21.42%), Space: 16.7 MB (16.43%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -16,3 +16,54 @@ class Solution:
                 stack.pop()
         backtrack(0, 0)
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # res = []
+        # stack = []
+        # def backtrack(nopen, nclose):
+        #     if nopen == nclose == n:
+        #         res.append(''.join(stack))
+        #         return
+        #     if nopen < n:
+        #         stack.append('(')
+        #         backtrack(nopen+1, nclose)
+        #         stack.pop()
+        #     if nopen > nclose:
+        #         stack.append(')')
+        #         backtrack(nopen, nclose+1)
+        #         stack.pop()
+        # backtrack(0, 0)
+        # return res
