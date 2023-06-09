af642f2a4fe9554ff7f9bc945d44474936c7c188
Daniel L <datadote@gmail.com>
2023-06-09 14:34:23 -0700

    Time: 69 ms (7.41%), Space: 16.5 MB (68.10%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,15 +1,61 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        L, R = 0, len(nums) - 1
-        min_num = math.inf
+        min_v = math.inf
+        L, R = 0, len(nums)-1
         while L <= R:
             M = L + (R-L)//2
-            min_num = min(min_num, nums[M])
-            # print(min_num, M, nums[M])
-            if nums[M]>=nums[L] and nums[L]<nums[R]:
-                R = M - 1
-            elif nums[M]<=nums[R] and nums[L]>nums[R]:
-                R = M - 1
-            else:
+            min_v = min(min_v, nums[M])
+            if nums[M] > nums[R]:
                 L = M + 1
-        return min_num
+            else:
+                R = M - 1
+        return min_v
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # L, R = 0, len(nums) - 1
+        # min_num = math.inf
+        # while L <= R:
+        #     M = L + (R-L)//2
+        #     min_num = min(min_num, nums[M])
+        #     # print(min_num, M, nums[M])
+        #     if nums[M]>=nums[L] and nums[L]<nums[R]:
+        #         R = M - 1
+        #     elif nums[M]<=nums[R] and nums[L]>nums[R]:
+        #         R = M - 1
+        #     else:
+        #         L = M + 1
+        # return min_num
