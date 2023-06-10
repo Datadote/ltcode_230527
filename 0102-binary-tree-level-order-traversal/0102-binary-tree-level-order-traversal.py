e20d3206413a84104f0523ef59a373a50572b9d6
Daniel L <datadote@gmail.com>
2023-06-10 14:05:27 -0700

    Time: 55 ms (40.24%), Space: 16.9 MB (51.14%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -22,6 +22,58 @@ class Solution:
             res.append(tmp)
         return res
         
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # res = []
+        # dq = collections.deque()
+        # if root:
+        #     dq.append(root)
+        # while dq:
+        #     tmp = []
+        #     for i in range(len(dq)):
+        #         node = dq.popleft()
+        #         tmp.append(node.val)
+        #         if node.left:
+        #             dq.append(node.left)
+        #         if node.right:
+        #             dq.append(node.right)
+        #     res.append(tmp)
+        # return res
+        
         # res = []
         # dq = collections.deque([root])
         # while len(dq) > 0:
