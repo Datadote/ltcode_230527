9259082f85960db922109ac92e85f3f0b8a43dd8
Daniel L <datadote@gmail.com>
2023-06-09 23:57:32 -0700

    Time: 60 ms (18.24%), Space: 17.1 MB (21.15%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -7,17 +7,33 @@
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         res = []
-        dq = collections.deque([root])
-        while len(dq) > 0:
+        dq = collections.deque()
+        if root:
+            dq.append(root)
+        while dq:
             tmp = []
             for i in range(len(dq)):
-                troot = dq.popleft()
-                if troot:
-                    tmp.append(troot.val)
-                if troot and troot.left:
-                    dq.append(troot.left)
-                if troot and troot.right:
-                    dq.append(troot.right)
-            if tmp:
-                res.append(tmp)
+                node = dq.popleft()
+                tmp.append(node.val)
+                if node.left:
+                    dq.append(node.left)
+                if node.right:
+                    dq.append(node.right)
+            res.append(tmp)
         return res
+        
+        # res = []
+        # dq = collections.deque([root])
+        # while len(dq) > 0:
+        #     tmp = []
+        #     for i in range(len(dq)):
+        #         troot = dq.popleft()
+        #         if troot:
+        #             tmp.append(troot.val)
+        #         if troot and troot.left:
+        #             dq.append(troot.left)
+        #         if troot and troot.right:
+        #             dq.append(troot.right)
+        #     if tmp:
+        #         res.append(tmp)
+        # return res
