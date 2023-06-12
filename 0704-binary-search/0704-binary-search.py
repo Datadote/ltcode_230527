bf4ef0e0a513fa19cc64bef38a11c26a6e79d949
Daniel L <datadote@gmail.com>
2023-06-12 09:09:32 -0700

    Time: 247 ms (50.52%), Space: 18 MB (20.44%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -3,13 +3,57 @@ class Solution:
         L, R = 0, len(nums)-1
         while L <= R:
             M = L + (R-L)//2
-            if nums[M] > target:
-                R = M - 1
-            elif nums[M] < target:
+            if target > nums[M]:
                 L = M + 1
+            elif target < nums[M]:
+                R = M - 1
             else:
                 return M
         return -1
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # L, R = 0, len(nums)-1
+        # while L <= R:
+        #     M = L + (R-L)//2
+        #     if nums[M] > target:
+        #         R = M - 1
+        #     elif nums[M] < target:
+        #         L = M + 1
+        #     else:
+        #         return M
+        # return -1
     
         # L, R = 0, len(nums)-1
         # while L <= R:
