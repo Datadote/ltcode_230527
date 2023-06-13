5abf06d572997af4e1bc689ed6e36f08ad6be287
Daniel L <datadote@gmail.com>
2023-06-12 21:26:24 -0700

    Time: 105 ms (10.39%), Space: 20.9 MB (14.87%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,11 +7,22 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        cur = root
-        while cur:
-            if cur.val < p.val and cur.val < q.val:
-                cur = cur.right
-            elif cur.val > p.val and cur.val > q.val:
-                cur = cur.left
-            else:
-                return cur
+        def dfs(cur, p, q):
+            root = cur
+            while root:
+                if p.val > root.val and q.val > root.val:
+                    root = root.right
+                elif p.val < root.val and q.val < root.val:
+                    root = root.left
+                else:
+                    return root
+        return dfs(root, p, q)
+
+        # cur = root
+        # while cur:
+        #     if cur.val < p.val and cur.val < q.val:
+        #         cur = cur.right
+        #     elif cur.val > p.val and cur.val > q.val:
+        #         cur = cur.left
+        #     else:
+        #         return cur
