3aac48ab3945b28f7cc2a2f87dc81444d4536b30
Daniel L <datadote@gmail.com>
2023-06-12 22:28:10 -0700

    Time: 58 ms (19.96%), Space: 16.1 MB (79.02%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -14,7 +14,6 @@ class Solution:
                 tail.next = list2
                 list2 = list2.next
             tail = tail.next
-        
         if list1:
             tail.next = list1
         if list2:
@@ -54,6 +53,23 @@ class Solution:
         
         
         
+        
+#         dummy = tail = ListNode()
+#         while list1 and list2:
+#             if list1.val <= list2.val:
+#                 tail.next = list1
+#                 list1 = list1.next
+#             else:
+#                 tail.next = list2
+#                 list2 = list2.next
+#             tail = tail.next
+        
+#         if list1:
+#             tail.next = list1
+#         if list2:
+#             tail.next = list2
+#         return dummy.next
+
 #         dummy = tail = ListNode()
 #         while list1 and list2:
 #             if list1.val < list2.val:
