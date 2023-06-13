d436d510f50114b1018ec3d01bb4cb5f6299c81c
Daniel L <datadote@gmail.com>
2023-06-13 13:22:34 -0700

    Time: 70 ms (29.96%), Space: 16.8 MB (85.35%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,12 +1,14 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        alp_num = set('abcdefghijklmnopqrstuvwrxyz0123456789')
+        alp_num = set('abcdefghijklmnopqrstuvwxyz0123456789')
         L, R = 0, len(s)-1
         while L < R:
-            while L<R and s[L].lower() not in alp_num:
+            if s[L].lower() not in alp_num:
                 L += 1
-            while L<R and s[R].lower() not in alp_num:
+                continue
+            if s[R].lower() not in alp_num:
                 R -= 1
+                continue
             if s[L].lower() != s[R].lower():
                 return False
             L += 1
@@ -45,6 +47,21 @@ class Solution:
         
         
         
+        
+        
+        # alp_num = set('abcdefghijklmnopqrstuvwrxyz0123456789')
+        # L, R = 0, len(s)-1
+        # while L < R:
+        #     while L<R and s[L].lower() not in alp_num:
+        #         L += 1
+        #     while L<R and s[R].lower() not in alp_num:
+        #         R -= 1
+        #     if s[L].lower() != s[R].lower():
+        #         return False
+        #     L += 1
+        #     R -= 1
+        # return True
+
         # alp_num = set('abcdefghijklmnopqrstuvwxyz0123456789')
         # L, R = 0, len(s)-1
         # while L <= R:
