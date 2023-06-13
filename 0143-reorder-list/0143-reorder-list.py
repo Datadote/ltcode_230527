69a00f5e046ba57e83169e414a16a767fa83c3d8
Daniel L <datadote@gmail.com>
2023-06-13 12:46:30 -0700

    Time: 103 ms (54.09%), Space: 26.1 MB (57.42%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -15,19 +15,74 @@ class Solution:
         second = slow.next
         slow.next = None
         
-        # Reverse 2nd half, prev + second
-        prev = None
+        prev = None # second == cur
         while second:
             nxt = second.next
             second.next = prev
             prev = second
             second = nxt
-        second = prev
         
-        # merge
-        while second:
-            nxt1, nxt2 = head.next, second.next
-            head.next = second
-            second.next = nxt1
-            head = nxt1
-            second = nxt2
+        list1, list2 = head, prev
+        while list2:
+            tmp1, tmp2 = list1.next, list2.next
+            list1.next = list2
+            list2.next = tmp1
+            list1 = tmp1
+            list2 = tmp2
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         slow, fast = head, head.next
+#         while fast and fast.next:
+#             slow = slow.next
+#             fast = fast.next.next
+#         second = slow.next
+#         slow.next = None
+        
+#         # Reverse 2nd half, prev + second
+#         prev = None
+#         while second:
+#             nxt = second.next
+#             second.next = prev
+#             prev = second
+#             second = nxt
+#         second = prev
+        
+#         # merge
+#         while second:
+#             nxt1, nxt2 = head.next, second.next
+#             head.next = second
+#             second.next = nxt1
+#             head = nxt1
+#             second = nxt2
