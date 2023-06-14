4349dffe6a8a895f6ca83038690fc4fe156a5d10
Daniel L <datadote@gmail.com>
2023-06-14 12:47:46 -0700

    Time: 81 ms (41.65%), Space: 16.6 MB (16.45%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -3,17 +3,21 @@ class Solution:
         res = []
         tmp = []
         def dfs(total, i):
-            # if i >= len(candidates):
-            #     return
+            if i >= len(candidates):
+                return
             if total == target:
                 res.append(tmp.copy())
                 return
             elif total > target:
                 return
             else:
-                for j in range(i, len(candidates)):
-                    tmp.append(candidates[j])
-                    dfs(total + candidates[j], j)
-                    tmp.pop()
+                tmp.append(candidates[i])
+                dfs(total + candidates[i], i)
+                tmp.pop()
+                dfs(total, i+1)
+                # for j in range(i, len(candidates)):
+                #     tmp.append(candidates[j])
+                #     dfs(total + candidates[j], j)
+                #     tmp.pop()
         dfs(0, 0)
         return res
