c902608cdce1daff5ff1324f7ad5fa9a1c2a643e
Daniel L <datadote@gmail.com>
2023-06-14 11:43:33 -0700

    Time: 46 ms (19.95%), Space: 16.3 MB (20.55%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -45,6 +45,17 @@ class Solution:
         
         
         
+        
+        # stones = [-x for x in stones]
+        # heapq.heapify(stones)
+        # while len(stones) > 1:
+        #     large = -heapq.heappop(stones)
+        #     small = -heapq.heappop(stones)
+        #     diff = large - small
+        #     if diff > 0:
+        #         heapq.heappush(stones, -diff)
+        # return -stones[0] if stones else 0
+        
         # stones = [-x for x in stones]
         # heapq.heapify(stones) # O(n)
         # while len(stones) > 1:
