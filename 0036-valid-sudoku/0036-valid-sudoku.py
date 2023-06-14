624a4af5f6e9aa4dec4a28f444c5a1a51a73ba4a
Daniel L <datadote@gmail.com>
2023-06-14 09:48:34 -0700

    Time: 125 ms (19.88%), Space: 16.3 MB (38.03%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,21 +4,78 @@ class Solution:
         dr = defaultdict(set)
         dc = defaultdict(set)
         db = defaultdict(set)
-        
-        for row in range(ROWS):
-            for col in range(COLS):
-                v = board[row][col]
-                if v == '.':
+        for r in range(ROWS):
+            for c in range(COLS):
+                num = board[r][c]
+                if num == '.':
                     continue
-                if (v in dr[row]
-                    or v in dc[col]
-                    or v in db[(row//3, col//3)]
+                if (num in dr[r]
+                    or num in dc[c]
+                    or num in db[(r//3, c//3)]
                    ):
                     return False
-                dr[row].add(v)
-                dc[col].add(v)
-                db[(row//3, col//3)].add(v)
+                dr[r].add(num)
+                dc[c].add(num)
+                db[(r//3, c//3)].add(num)
         return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         ROWS, COLS = len(board), len(board[0])
+#         dr = defaultdict(set)
+#         dc = defaultdict(set)
+#         db = defaultdict(set)
+        
+#         for row in range(ROWS):
+#             for col in range(COLS):
+#                 v = board[row][col]
+#                 if v == '.':
+#                     continue
+#                 if (v in dr[row]
+#                     or v in dc[col]
+#                     or v in db[(row//3, col//3)]
+#                    ):
+#                     return False
+#                 dr[row].add(v)
+#                 dc[col].add(v)
+#                 db[(row//3, col//3)].add(v)
+#         return True
 
         # Can be done with defaultdict, set. Would use add instead of append
         # ROWS, COLS = len(board), len(board[0])
