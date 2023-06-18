0042d54ef04a8747f98622acfc34ac71ec84fc0e
Daniel L <datadote@gmail.com>
2023-06-18 12:38:56 -0700

    Time: 1501 ms (57.93%), Space: 20.4 MB (63.28%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -8,14 +8,14 @@ class Solution:
             L, R = i+1, len(nums)-1
             while L < R:
                 total = nums[i] + nums[L] + nums[R]
-                if total > 0:
-                    R -= 1
-                elif total < 0:
+                if total < 0:
                     L += 1
+                elif total > 0:
+                    R -= 1
                 else:
                     res.append([nums[i], nums[L], nums[R]])
                     L += 1
-                    while L < R and nums[L] == nums[L-1]:
+                    while L<R and nums[L]==nums[L-1]:
                         L += 1
         return res
         
@@ -53,6 +53,26 @@ class Solution:
         
         
         
+        
+        # res = []
+        # nums.sort()
+        # for i in range(len(nums)-2):
+        #     if i and nums[i]==nums[i-1]:
+        #         continue
+        #     L, R = i+1, len(nums)-1
+        #     while L < R:
+        #         total = nums[i] + nums[L] + nums[R]
+        #         if total > 0:
+        #             R -= 1
+        #         elif total < 0:
+        #             L += 1
+        #         else:
+        #             res.append([nums[i], nums[L], nums[R]])
+        #             L += 1
+        #             while L < R and nums[L] == nums[L-1]:
+        #                 L += 1
+        # return res
+
         # res = []
         # nums.sort()
         # for i in range(len(nums)-2):
