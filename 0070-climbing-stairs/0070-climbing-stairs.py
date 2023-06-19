04af7a38e40d5c29db85e08a537e09db35d3082c
Daniel L <datadote@gmail.com>
2023-06-18 19:46:11 -0700

    Time: 48 ms (36.66%), Space: 16.3 MB (57.88%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,10 +1,20 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        d = {1:1, 2:2}
-        def fibo(n):
-            if n in d:
-                return d[n]
-            tmp = fibo(n-1) + fibo(n-2)
-            d[n] = tmp
-            return tmp
-        return fibo(n)
+        def fibo2(n):
+            if n==1 or n==2:
+                return n
+            n1, n2 = 1, 2
+            for i in range(n-2):
+                n1, n2 = n2, n1+n2
+            return n2
+        return fibo2(n)
+            
+        # # T: O(n), S: O(n)
+        # d = {1:1, 2:2}
+        # def fibo(n):
+        #     if n in d:
+        #         return d[n]
+        #     tmp = fibo(n-1) + fibo(n-2)
+        #     d[n] = tmp
+        #     return tmp
+        # return fibo(n)
