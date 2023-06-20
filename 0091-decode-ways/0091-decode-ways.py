9eb658c481cd42b2ea148384c138bab8c86ea4d3
Daniel L <datadote@gmail.com>
2023-06-19 20:13:49 -0700

    Time: 47 ms (62.48%), Space: 16.6 MB (10.02%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -26,7 +26,8 @@ class Solution:
                 return 0
             tmp = dfs(i+1)
             if (i < len(s) - 1
-                and (s[i]=='1' or s[i]=='2' and s[i+1] in '0123456')
+                # 10-19 are double digit, 20-26 are other case
+                and (s[i]=='1' or (s[i]=='2' and s[i+1] in '0123456'))
                ):
                 tmp += dfs(i+2)
             dp[i] = tmp
