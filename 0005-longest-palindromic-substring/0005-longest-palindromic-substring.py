ca7a514780404e6b2b598f8dc288b5989a7d88a0
Daniel L <datadote@gmail.com>
2023-06-21 11:21:01 -0700

    Time: 823 ms (47.06%), Space: 16.5 MB (26.42%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,20 +1,72 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         res = ''
-        max_len = 0
         for i in range(len(s)):
             L, R = i, i
-            while (L >= 0) and (R < len(s)) and (s[L]==s[R]):
-                if R-L+1 > max_len:
-                    max_len = R - L + 1
+            while L>=0 and R<len(s) and s[L]==s[R]:
+                if (R-L+1) > len(res):
                     res = s[L:R+1]
                 L -= 1
                 R += 1
             L, R = i, i+1
-            while (L >= 0) and (R < len(s)) and (s[L]==s[R]):
-                if R-L+1 > max_len:
-                    max_len = R - L + 1
+            while L>=0 and R<len(s) and s[L]==s[R]:
+                if (R-L+1) > len(res):
                     res = s[L:R+1]
                 L -= 1
                 R += 1
         return res
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # res = ''
+        # max_len = 0
+        # for i in range(len(s)):
+        #     L, R = i, i
+        #     while (L >= 0) and (R < len(s)) and (s[L]==s[R]):
+        #         if R-L+1 > max_len:
+        #             max_len = R - L + 1
+        #             res = s[L:R+1]
+        #         L -= 1
+        #         R += 1
+        #     L, R = i, i+1
+        #     while (L >= 0) and (R < len(s)) and (s[L]==s[R]):
+        #         if R-L+1 > max_len:
+        #             max_len = R - L + 1
+        #             res = s[L:R+1]
+        #         L -= 1
+        #         R += 1
+        # return res
