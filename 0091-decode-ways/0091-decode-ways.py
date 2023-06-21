6fe664cb5ca3daa949c0650fda65f15701fd1fcb
Daniel L <datadote@gmail.com>
2023-06-21 14:08:35 -0700

    Time: 56 ms (16.04%), Space: 16.6 MB (10.31%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,23 +1,5 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-#         dp = {len(s): 1}
-
-#         def dfs(i):
-#             if i in dp:
-#                 return dp[i]
-#             if s[i] == "0":
-#                 return 0
-
-#             res = dfs(i + 1)
-#             if i + 1 < len(s) and (
-#                 s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456"
-#             ):
-#                 res += dfs(i + 2)
-#             dp[i] = res
-#             return res
-
-#         return dfs(0)
-        
         dp = {len(s): 1}
         def dfs(i):
             if i in dp:
@@ -25,11 +7,26 @@ class Solution:
             if s[i] == '0':
                 return 0
             tmp = dfs(i+1)
-            if (i < len(s) - 1
-                # 10-19 are double digit, 20-26 are other case
-                and ((s[i]=='1') or (s[i]=='2' and s[i+1] in '0123456'))
+            if (i < len(s)-1
+                and ((s[i] == '1') or (s[i] == '2' and s[i+1] in '0123456'))
                ):
                 tmp += dfs(i+2)
             dp[i] = tmp
             return tmp
         return dfs(0)
+        
+        # dp = {len(s): 1}
+        # def dfs(i):
+        #     if i in dp:
+        #         return dp[i]
+        #     if s[i] == '0':
+        #         return 0
+        #     tmp = dfs(i+1)
+        #     if (i < len(s) - 1
+        #         # 10-19 are double digit, 20-26 are other case
+        #         and ((s[i]=='1') or (s[i]=='2' and s[i+1] in '0123456'))
+        #        ):
+        #         tmp += dfs(i+2)
+        #     dp[i] = tmp
+        #     return tmp
+        # return dfs(0)
