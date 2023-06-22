a264388d29ac5bcd75575b7fd11942ff6016d2dc
Daniel L <datadote@gmail.com>
2023-06-22 12:33:14 -0700

    Time: 47 ms (65.44%), Space: 16.4 MB (30.58%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -8,12 +8,67 @@ class Solution:
                 return 0
             tmp = dfs(i+1)
             if (i < len(s)-1
-                and ((s[i] == '1') or (s[i] == '2' and s[i+1] in '0123456'))
+                and (s[i]=='1' or (s[i]=='2' and s[i+1] in '0123456'))
                ):
                 tmp += dfs(i+2)
             dp[i] = tmp
             return tmp
         return dfs(0)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # dp = {len(s): 1}
+        # def dfs(i):
+        #     if i in dp:
+        #         return dp[i]
+        #     if s[i] == '0':
+        #         return 0
+        #     tmp = dfs(i+1)
+        #     if (i < len(s)-1
+        #         and ((s[i] == '1') or (s[i] == '2' and s[i+1] in '0123456'))
+        #        ):
+        #         tmp += dfs(i+2)
+        #     dp[i] = tmp
+        #     return tmp
+        # return dfs(0)
         
         # dp = {len(s): 1}
         # def dfs(i):
