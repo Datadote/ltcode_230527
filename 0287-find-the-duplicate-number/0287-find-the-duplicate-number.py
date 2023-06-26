b277270912da5889a70f6f510902530df55dc83a
Daniel L <datadote@gmail.com>
2023-06-26 14:24:54 -0700

    Time: 623 ms (51.94%), Space: 31.2 MB (66.34%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -5,11 +5,63 @@ class Solution:
             slow = nums[slow]
             fast = nums[fast]
             fast = nums[fast]
-            if nums[slow] == nums[fast]:
+            if slow == fast:
                 break
         slow2 = 0
         while True:
-            slow2 = nums[slow2]
             slow = nums[slow]
+            slow2 = nums[slow2]
             if slow == slow2:
-                return slow2
+                return slow
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # slow = fast = 0
+        # while True:
+        #     slow = nums[slow]
+        #     fast = nums[fast]
+        #     fast = nums[fast]
+        #     if nums[slow] == nums[fast]:
+        #         break
+        # slow2 = 0
+        # while True:
+        #     slow2 = nums[slow2]
+        #     slow = nums[slow]
+        #     if slow == slow2:
+        #         return slow2
