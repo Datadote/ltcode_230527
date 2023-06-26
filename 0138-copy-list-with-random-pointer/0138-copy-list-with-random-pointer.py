a1254787873e098662480de0fafa16fa6a7dc057
Daniel L <datadote@gmail.com>
2023-06-26 11:33:15 -0700

    Time: 53 ms (56.30%), Space: 17.2 MB (95.70%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,15 +9,63 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        old2new = {None: None}
-        cur = head
-        while cur:
-            old2new[cur] = Node(cur.val)
-            cur = cur.next
-        cur = head
-        while cur:
-            copy = old2new[cur]
-            copy.next = old2new[cur.next]
-            copy.random = old2new[cur.random]
-            cur = cur.next
-        return old2new[head]
+        do2n = {None: None}
+        curr = head
+        while curr:
+            do2n[curr] = Node(curr.val)
+            curr = curr.next
+        curr = head
+        while curr:
+            newNode = do2n[curr]
+            newNode.next = do2n[curr.next]
+            newNode.random = do2n[curr.random]
+            curr = curr.next
+        return do2n[head]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # old2new = {None: None}
+        # cur = head
+        # while cur:
+        #     old2new[cur] = Node(cur.val)
+        #     cur = cur.next
+        # cur = head
+        # while cur:
+        #     copy = old2new[cur]
+        #     copy.next = old2new[cur.next]
+        #     copy.random = old2new[cur.random]
+        #     cur = cur.next
+        # return old2new[head]
