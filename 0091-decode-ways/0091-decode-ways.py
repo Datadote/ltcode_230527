bea3275afe295cb46b899fc088738ca718b8e859
Daniel L <datadote@gmail.com>
2023-06-26 21:39:46 -0700

    Time: 49 ms (57.11%), Space: 16.6 MB (20.64%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -4,6 +4,8 @@ class Solution:
         def dfs(i):
             if i in dp:
                 return dp[i]
+            if i == len(s):
+                return 1
             if s[i] == '0':
                 return 0
             tmp = dfs(i+1)
@@ -48,12 +50,20 @@ class Solution:
         
         
         
-        
-        
-        
-        
-        
-        
+        # dp = {len(s): 1}
+        # def dfs(i):
+        #     if i in dp:
+        #         return dp[i]
+        #     if s[i] == '0':
+        #         return 0
+        #     tmp = dfs(i+1)
+        #     if (i < len(s)-1
+        #         and (s[i]=='1' or (s[i]=='2' and s[i+1] in '0123456'))
+        #        ):
+        #         tmp += dfs(i+2)
+        #     dp[i] = tmp
+        #     return tmp
+        # return dfs(0)
         
         # dp = {len(s): 1}
         # def dfs(i):
