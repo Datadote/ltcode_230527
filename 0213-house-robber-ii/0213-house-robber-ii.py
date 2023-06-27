3e281751424cfa33cbf62d43bae75598c07a1304
Daniel L <datadote@gmail.com>
2023-06-26 20:43:26 -0700

    Time: 44 ms (81.70%), Space: 16.4 MB (41.09%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,11 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        def rob1(nums):
-            rob1, rob2 = 0,0
+        def rob(nums):
+            dp0, dp1 = 0, 0
             for n in nums:
-                rob1, rob2 = rob2, max(rob1+n, rob2)
-            return rob2
-        return max(rob1(nums[:-1]), rob1(nums[1:]), nums[0])
+                dp0, dp1 = dp1, max(dp1, dp0+n)
+            return dp1
+        
+        return max(rob(nums[1:]), rob(nums[:-1]), nums[0])
         
         
         
@@ -40,6 +41,17 @@ class Solution:
         
         
         
+        
+        
+        
+        
+        
+        # def rob1(nums):
+        #     rob1, rob2 = 0,0
+        #     for n in nums:
+        #         rob1, rob2 = rob2, max(rob1+n, rob2)
+        #     return rob2
+        # return max(rob1(nums[:-1]), rob1(nums[1:]), nums[0])
         
         # def rob1(nums):
         #     dp0, dp1 = 0, 0
